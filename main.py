77c072aedaa569d8f00a4962c9e278a1860abe87
Mohamed Ismail <mohamed.tm.ismail@gmail.com>
2024-11-27 01:39:28 +0000

    now the triangle rotates using a and d

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from player import *
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60)/1000
 
 
--- player.py
+++ player.py
@@ -3,12 +3,10 @@ from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
-    
     def __init__(self, x, y):
         CircleShape.__init__(self, x, y, PLAYER_RADIUS)
         self.rotation = 0
     
-    # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -20,4 +18,13 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
         
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt 
     
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
