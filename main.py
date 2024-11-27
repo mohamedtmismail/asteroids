3b69a5650909469988682c5f03069a034dd7af07
Mohamed Ismail <mohamed.tm.ismail@gmail.com>
2024-11-27 01:24:54 +0000

    added triangle in the middle

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
